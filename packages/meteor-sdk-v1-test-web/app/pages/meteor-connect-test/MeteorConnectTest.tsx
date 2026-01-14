de9ac897c5e167114ec106e97e836ef9f272cb68
Paul Myburgh <lostpebble@protonmail.com>
2026-01-14 05:04:39 +0200

    feat: tested sign message working

--- packages/meteor-sdk-v1-test-web/app/pages/meteor-connect-test/MeteorConnectTest.tsx
+++ packages/meteor-sdk-v1-test-web/app/pages/meteor-connect-test/MeteorConnectTest.tsx
@@ -1,8 +1,10 @@
 import { MeteorConnect } from "@meteorwallet/sdk";
+import type { IMCActionDef_Near_SignMessage } from "@meteorwallet/sdk/MeteorConnect/action/mc_action.near.types.ts";
 import type { IMeteorConnectAccount } from "@meteorwallet/sdk/MeteorConnect/MeteorConnect.types.ts";
 import { webpage_local_storage } from "@meteorwallet/sdk/ported_common/utils/storage/webpage/webpage_local_storage.ts";
 import { QueryClient, QueryClientProvider, useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { createSimpleNonce, GUESTBOOK_CONTRACT_ID } from "~/pages/meteor-sdk-test/guestbook.ts";
 import { NetworkSelector } from "~/pages/near-connect/NetworkSelector.tsx";
 import { Button } from "~/ui/Button.tsx";
 
@@ -91,7 +93,7 @@ const MeteorConnectTestInitialized = ({ meteorConnect }: { meteorConnect: Meteor
             onClick={async () => {
               await meteorConnect.actionRequest({
                 actionId: "near::sign_out",
-                accountIdentifier: account.identifier,
+                target: account.identifier,
               });
 
               await accountQuery.refetch({ cancelRefetch: true });
@@ -99,22 +101,48 @@ const MeteorConnectTestInitialized = ({ meteorConnect }: { meteorConnect: Meteor
           >
             Sign Out
           </Button>
-          <MeteorConnectWithAccount account={account} />
+          <MeteorConnectWithAccount account={account} meteorConnect={meteorConnect} />
         </>
       )}
     </div>
   );
 };
 
-const MeteorConnectWithAccount = ({ account }: { account: IMeteorConnectAccount }) => {
+const MeteorConnectWithAccount = ({
+  account,
+  meteorConnect,
+}: {
+  account: IMeteorConnectAccount;
+  meteorConnect: MeteorConnect;
+}) => {
   const mutate_signMessage = useMutation({
     mutationKey: ["mutate_signMessage", account.identifier, account.publicKeys],
-    mutationFn: async () => {},
+    mutationFn: async () => {
+      const response = await meteorConnect.actionRequest<IMCActionDef_Near_SignMessage>({
+        actionId: "near::sign_message",
+        messageParams: {
+          message: "hello",
+          nonce: createSimpleNonce(),
+          recipient: GUESTBOOK_CONTRACT_ID,
+        },
+        target: account.identifier,
+      });
+
+      return response.outcome;
+    },
   });
 
   return (
     <div className={"p-5"}>
       <h1>{account.identifier.accountId} Signed In</h1>
+      <Button
+        onClick={async () => {
+          const signedMessage = await mutate_signMessage.mutateAsync();
+          console.log(signedMessage);
+        }}
+      >
+        Sign Message
+      </Button>
     </div>
   );
 };
